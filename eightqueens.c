71b836b233c3bca96a0a906b8b177a35878a51a4
Marcio <siqdn@yahoo.ca>
2020-01-14 17:54:49 -0500

    Just variable names and comments.

--- eightqueens.c
+++ eightqueens.c
@@ -1,17 +1,32 @@
+//marciosdn
+//2020-01-14
+
 #include <stdio.h>
+
+/*
+Array solution 15863724 means:
+10000000
+00005000
+00000008
+00000600
+00300000
+00000070
+02000000
+00040000
+*/
 
 #define SIZE 8
 
-int t[SIZE] = {0,0,0,0,0,0,0,0};
-int q=1;
-int a=1;
+int A[SIZE] = {0,0,0,0,0,0,0,0};    //array solution
+int s=1;    //solutions
+int t=1;    //total interations
 
 int test(int queen,int col) {
-	a++;
+	t++;
 	for (int i=1;i<queen;i++) {
-		if (t[queen-i-1]==col) return 0;
-		if (t[queen-i-1]==col-i) return 0;
-		if (t[queen-i-1]==col+i) return 0;
+		if (A[queen-i-1]==col) return 0;
+		if (A[queen-i-1]==col-i) return 0;
+		if (A[queen-i-1]==col+i) return 0;
 	}
 	return 1;
 }
@@ -19,15 +34,15 @@ int test(int queen,int col) {
 void play(int queen) {
 	for (int col=1;col<SIZE+1;col++) {
 		if (test(queen,col)) {
-			t[queen-1]=col;
-			if(queen==SIZE) printf("%d,%d,%d%d%d%d%d%d%d%d\n",q++,a,t[0],t[1],t[2],t[3],t[4],t[5],t[6],t[7]);
+			A[queen-1]=col;
+			if(queen==SIZE) printf("Solution: %d, %d, %d%d%d%d%d%d%d%d\n",s++,t,A[0],A[1],A[2],A[3],A[4],A[5],A[6],A[7]);
 			else play(queen+1);
-			t[queen-1]=0;
+			A[queen-1]=0;
 		}
 	}
 }
 
 void main(void) {
 	play(1);
-	printf("%d",a);
+	printf("Total %d",t);
 }
